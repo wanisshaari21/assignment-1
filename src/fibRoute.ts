9d2fd212123db3a95b0478b92ff4bb3526bc1928
wanisshaari21 <wa852145@ucf.edu>
2026-01-20 12:53:36 -0500

    Fix TypeScript errors in fibRoute

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,12 +1,14 @@
 // Endpoint for querying the fibonacci numbers
-
+import { Request, Response } from "express";
 import fibonacci from "./fib";
 
-export default (req, res) => {
-  const { num } = req.params;
+export default (req: Request, res: Response): void => {
+  const num: string = req.params.num;
 
-  const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+  const n: number = parseInt(num, 10);
+  const fibN: number = fibonacci(n);
+
+  let result: string = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
