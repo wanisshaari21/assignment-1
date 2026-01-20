c37a2b47cb4ba6be923c789a7d21db92fbfb855d
wanisshaari21 <wa852145@ucf.edu>
2026-01-20 14:03:20 -0500

    Fix left errors in fib.ts

--- src/fib.ts
+++ src/fib.ts
@@ -2,9 +2,9 @@
 export default function fibonacci(n: number): number {
   if (n < 0) {
     return -1;
-  } else if (n == 0) {
+  } else if (n === 0) {
     return 0;
-  } else if (n == 1) {
+  } else if (n === 1) {
     return 1;
   }
 
