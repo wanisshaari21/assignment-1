2f251db489aa9913069a6943a4812a9ccda39214
wanisshaari21 <wa852145@ucf.edu>
2026-01-20 13:38:43 -0500

    Fix rest of errors in fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -3,12 +3,12 @@ import { Request, Response } from "express";
 import fibonacci from "./fib";
 
 export default (req: Request, res: Response): void => {
-  const num: string = req.params.num;
+  const num = req.params.num as string;
 
   const n: number = parseInt(num, 10);
   const fibN: number = fibonacci(n);
 
-  let result: string = `fibonacci(${num}) is ${fibN}`;
+  let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
